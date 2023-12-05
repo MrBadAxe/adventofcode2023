d2a868ba3b87f82ef35623c42753b3322bbcb455
MrBadAxe <mrbadaxe@gmail.com>
2023-12-04 21:22:18 -0500

    day 04: instead of calculating card value as part of the method, just calculate number of matches, then calculate card value as part of getPart01

--- Day04.java
+++ Day04.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.HashSet;
 public class Day04{
 
-  public static int getCardValue(String str){
+  public static int getCardMatchingValuesCount(String str){
     String card = str.split(":")[1];
     String[] firstHalf = card.split("\\|")[0].strip().split(" ");
     HashSet<Integer> winningNumbers = new HashSet<Integer>();
@@ -14,18 +14,21 @@ public class Day04{
     String[] secondHalf = card.split("\\|")[1].strip().split(" ");
     int numbersYouHave = 0;
     for(String num : secondHalf){
-      System.out.println(num);
       if(num != "" && winningNumbers.contains(Integer.parseInt(num))){
         numbersYouHave++;
       }
     }
-    return (numbersYouHave == 0 ? 0 : (int)Math.pow(2,(numbersYouHave-1)));
+    return numbersYouHave;
   }
 
   public static String getPart01(List<String> input){
     int total = 0;
     for(String str : input){
-      total += getCardValue(str);
+      int matches = getCardMatchingValuesCount(str);
+      total += (matches == 0 ? 0 : (int)Math.pow(2,matches-1));
+    }
+    return Integer.toString(total);
+  }
     }
     return Integer.toString(total);
   }
