d4d28185e05660eae93f203e7b4ca41ace5aa858
MrBadAxe <mrbadaxe@gmail.com>
2023-12-07 16:59:22 -0500

    day 07: card rank value is different for Jokers vs. Jacks, which also affects calculateHandRank

--- CamelPokerHand.java
+++ CamelPokerHand.java
@@ -8,34 +8,53 @@ public class CamelPokerHand implements Comparable<CamelPokerHand>{
 
   public CamelPokerHand(String str, int i, boolean jokers){
     this.hand = str.toUpperCase().replaceAll("[^2-9AJKQT]","").substring(0,5);
-    this.rank = calculateHandRank();
+    this.rank = calculateHandRank(jokers);
     this.value = i;
     this.hasJokers = jokers;
   }
 
-  private int getCardRankValue(char c){
-    switch(c){
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9': return c-'2';
-      case 'T': return 8;
-      case 'J': return 9;
-      case 'Q': return 10;
-      case 'K': return 11;
-      case 'A': return 12;
-      default:  return -1;
+  private int getCardRankValue(char c, boolean jokers){
+    if(jokers){
+      switch(c){
+        case 'J': return 0;
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9': return c-'1';
+        case 'T': return 9;
+        case 'Q': return 10;
+        case 'K': return 11;
+        case 'A': return 12;
+        default:  return -1;
+      }
+    }else{
+      switch(c){
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9': return c-'2';
+        case 'T': return 8;
+        case 'J': return 9;
+        case 'Q': return 10;
+        case 'K': return 11;
+        case 'A': return 12;
+        default:  return -1;
+      }
     }
   }
-  private Rank calculateHandRank(){
+  private Rank calculateHandRank(boolean jokers){
     int[] cardRanks = {0,0,0,0,0,0,0,0,0,0,0,0,0};
     for(int k=0;k<5;k++){
       char c = this.hand.charAt(k);
-      cardRanks[getCardRankValue(c)]++;
+      cardRanks[getCardRankValue(c,jokers)]++;
     }
     String groups = "";
     for(int k=0;k<13;k++){
@@ -91,7 +110,7 @@ public class CamelPokerHand implements Comparable<CamelPokerHand>{
       while(this.getHand().charAt(index) == other.getHand().charAt(index)){
         index++;
       }
-      return Integer.compare(getCardRankValue(this.getHand().charAt(index)),getCardRankValue(other.getHand().charAt(index)));
+      return Integer.compare(getCardRankValue(this.getHand().charAt(index),this.hasJokers()),getCardRankValue(other.getHand().charAt(index),other.hasJokers()));
     }
   }
 }
