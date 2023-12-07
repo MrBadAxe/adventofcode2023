dd6f99a98f37903da2f24ec09d924a532b6dbc29
MrBadAxe <mrbadaxe@gmail.com>
2023-12-07 12:46:45 -0500

    day 06: refactor

--- Day06.java
+++ Day06.java
@@ -1,21 +1,25 @@
 import java.util.List;
 
 public class Day06{
+  private static long getRaceNumWins(long time, long dist){
+    long lb = time/2;
+    long ub = time-lb;
+    while((lb * ub) > dist){
+      lb--;
+      ub++;
+    }
+    lb++;
+    ub--;
+    return (ub-lb+1);
+  }
+
   public static String getPart01(List<String> input){
     String[] raceMaxTimes = input.get(0).split(":")[1].strip().split("\s+");
     String[] raceBestDistances = input.get(1).split(":")[1].strip().split("\s+");
 
     int product = 1;
     for(int k=0;k<raceMaxTimes.length;k++){
-      int lb = Integer.parseInt(raceMaxTimes[k])/2;
-      int ub = Integer.parseInt(raceMaxTimes[k])-lb;
-      while((lb * ub) > Integer.parseInt(raceBestDistances[k])){
-        lb--;
-        ub++;
-      }
-      lb++;
-      ub--;
-      product *= (ub-lb+1);
+      product *= getRaceNumWins(Long.parseLong(raceMaxTimes[k]),Long.parseLong(raceBestDistances[k]));
     }
     return Integer.toString(product);
   }
@@ -23,16 +27,6 @@ public class Day06{
   public static String getPart02(List<String> input){
     String raceMaxTime = input.get(0).replaceAll("\s","").split(":")[1];
     String raceBestDistance = input.get(1).replaceAll("\s","").split(":")[1];
-
-    long lb = Long.parseLong(raceMaxTime)/2;
-    long ub = Long.parseLong(raceMaxTime)-lb;
-    while((lb * ub) > Long.parseLong(raceBestDistance)){
-      lb--;
-      ub++;
-    }
-    lb++;
-    ub--;
-
-    return Long.toString(ub-lb+1);
+    return Long.toString(getRaceNumWins(Long.parseLong(raceMaxTime),Long.parseLong(raceBestDistance)));
   }
 }
