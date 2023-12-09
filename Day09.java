87ebc4aee56496df3d6662baa85c95f89f4d63ff
MrBadAxe <mrbadaxe@gmail.com>
2023-12-09 01:26:49 -0500

    day 09: part 02

--- Day09.java
+++ Day09.java
@@ -4,16 +4,21 @@ public class Day09{
     int[] z = new int[sequence.length-1];
     for(int k=0;k<z.length;k++){
       z[k] = sequence[k+1] - sequence[k];
-      System.out.print(z[k] + " ");
     }
-    System.out.println("");
     return z;
   }
-  private static int extrapolate(int[] sequence){
+  private static int extrapolateRight(int[] sequence){
     if(sequence[0] == 0 && sequence[sequence.length-1] == 0){
       return 0;
     }else{
-      return sequence[sequence.length-1] + extrapolate(calculateDiffs(sequence));
+      return sequence[sequence.length-1] + extrapolateRight(calculateDiffs(sequence));
+    }
+  }
+  private static int extrapolateLeft(int[] sequence){
+    if(sequence[0] == 0 && sequence[sequence.length-1] == 0){
+      return 0;
+    }else{
+      return sequence[0] - extrapolateLeft(calculateDiffs(sequence));
     }
   }
 
@@ -26,7 +31,20 @@ public class Day09{
       for(int k=0;k<sequenceLength;k++){
         sequence[k] = Integer.parseInt(splitOnWhitespace[k]);
       }
-      total += extrapolate(sequence);
+      total += extrapolateRight(sequence);
+    }
+    return Integer.toString(total);
+  }
+  public static String getPart02(List<String> input){
+    int total = 0;
+    for(String str : input){
+      String[] splitOnWhitespace = str.split("\s");
+      int sequenceLength = splitOnWhitespace.length;
+      int[] sequence = new int[sequenceLength];
+      for(int k=0;k<sequenceLength;k++){
+        sequence[k] = Integer.parseInt(splitOnWhitespace[k]);
+      }
+      total += extrapolateLeft(sequence);
     }
     return Integer.toString(total);
   }
