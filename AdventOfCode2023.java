ec683a11564615f335cfa03d04a49243d74d176e
MrBadAxe <mrbadaxe@gmail.com>
2023-12-10 18:53:10 -0500

    day 10: part 01

--- AdventOfCode2023.java
+++ AdventOfCode2023.java
@@ -39,8 +39,10 @@ public class AdventOfCode2023{
 
     System.out.println("Day 08 Part 01: " + Day08.getPart01(readInput("./input_day08.txt")));
     System.out.println("Day 08 Part 02: " + Day08.getPart02(readInput("./input_day08.txt")));
-    */
+
     System.out.println("Day 09 Part 01: " + Day09.getPart01(readInput("./input_day09.txt")));
     System.out.println("Day 09 Part 02: " + Day09.getPart02(readInput("./input_day09.txt")));
+    */
+    System.out.println("Day 10 Part 01: " + Day10.getPart01(readInput("./input_day10.txt")));
   }
 }
---
+++ Day10.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Day10{
+  private static PipeGrid parsePipeMap(List<String> input){
+    PipeGrid map = new PipeGrid(input.size(),input.get(0).length());
+    for(int row=0;row<input.size();row++){
+      String line = input.get(row);
+      for(int col=0;col<line.length();col++){
+        map.set(row,col,line.charAt(col));
+      }
+    }
+    return map;
+  }
+
+  public static String getPart01(List<String> input){
+    PipeGrid map = parsePipeMap(input);
+    Point start = map.getStart();
+    List<Point> path = map.getAllPointsOnLoop();
+    return Integer.toString((path.size()-1)/2);
+  }
+}
