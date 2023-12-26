65c8ba340f4a54db70ee11d17936626d655c02de
MrBadAxe <mrbadaxe@gmail.com>
2023-12-26 17:11:29 -0500

    day 19: add methods to partition a PartRange into two smaller ranges

--- Workflow.java
+++ Workflow.java
@@ -17,6 +17,36 @@ public class Workflow{
     }
     return "";
   }
+  private ArrayList<PartRange> partition(PartRange range, WorkflowRule rule){
+    ArrayList<PartRange> z = new ArrayList<PartRange>();
+    if(rule.getThreshold() > range.getMinRating(rule.getRating()) && rule.getThreshold() < range.getMaxRating(rule.getRating())){
+      PartRange r1 = range.clone();
+      PartRange r2 = range.clone();
+      if(rule.getPolarity()){
+        r1.setMaxRating(rule.getRating(),rule.getThreshold());
+        r2.setMinRating(rule.getRating(),rule.getThreshold()+1);
+      }else{
+        r1.setMaxRating(rule.getRating(),rule.getThreshold()-1);
+        r2.setMinRating(rule.getRating(),rule.getThreshold());
+      }
+      z.add(r1);
+      z.add(r2);
+    }else{
+      z.add(range);
+    }
+    return z;
+  }
+  public ArrayList<PartRange> partition(PartRange range){
+    ArrayList<PartRange> z = new ArrayList<PartRange>();
+    z.add(range);
+    for(int k=0;k<rules.size() && z.size()<2;k++){
+      WorkflowRule rule = rules.get(k);
+      System.out.println(rule);
+      z = partition(z.get(0),rule);
+      System.out.println(k + " " + rules.size() + " " + z.size());
+    }
+    return z;
+  }
   public String toString(){
     String z = "";
     for(int k=0;k<rules.size();k++){
